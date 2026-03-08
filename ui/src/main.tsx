3c53ad4441dbe72cf8f98f2220a32b1097f384f5
lucasaugustodev <lucas@algoricca.com>
2026-03-08 10:18:33 -0300

    refactor(ui): simplify App import in main.tsx

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { App } from "./App.js";
+import App from "./App.js";
 import "./index.css";
 const qc = new QueryClient();
 createRoot(document.getElementById("root")!).render(
